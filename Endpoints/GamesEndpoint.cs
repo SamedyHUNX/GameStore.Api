90e5fd0e1aabb8e9c11748d283c7ac3150c724af
samedyhunx <samedy.hunx@gmail.com>
2026-01-09 01:16:46 +0700

    refractored endpoints to a separate file

---
+++ Endpoints/GamesEndpoint.cs
@@ -0,0 +1,80 @@
+using System;
+using GameStore.Api.Dtos;
+
+namespace GameStore.Api.Endpoints;
+
+public static class GamesEndpoint
+{
+    const string EndpointName = "GetGame";
+    private static readonly List<GameDto> games = [
+        new (1, "Street Fighter 2", "Fighting", 19.99M, new DateOnly(1992, 7, 15)),
+        new (2, "Placeholder Game A", "Action", 29.99M, new DateOnly(2000, 1, 1)),
+        new (3, "Placeholder Game B", "Adventure", 39.99M, new DateOnly(2001, 2, 2)),
+        new (4, "Placeholder Game C", "RPG", 49.99M, new DateOnly(2002, 3, 3)),
+        new (5, "Placeholder Game D", "Shooter", 59.99M, new DateOnly(2003, 4, 4)),
+        new (6, "Placeholder Game E", "Puzzle", 9.99M, new DateOnly(2004, 5, 5)),
+        new (7, "Placeholder Game F", "Sports", 19.99M, new DateOnly(2005, 6, 6)),
+        new (8, "Placeholder Game G", "Racing", 29.99M, new DateOnly(2006, 7, 7)),
+        new (9, "Placeholder Game H", "Strategy", 39.99M, new DateOnly(2007, 8, 8)),
+        new (10, "Placeholder Game I", "Simulation", 49.99M, new DateOnly(2008, 9, 9))
+    ];
+
+    public static void MapGamesEndpoints(this WebApplication app)
+    {
+        var group = app.MapGroup("/games");
+
+        group.MapGet("/", () => games);
+
+        // GET /games/1
+        group.MapGet("/{id}", (int id) =>
+        {
+            var game = games.Find(game => game.Id ==id);
+
+            return game is null ? Results.NotFound() : Results.Ok(game);
+        }).WithName(EndpointName);
+
+        group.MapPost("/", (CreateGameDto newGame) =>
+        {
+            GameDto game = new(
+                games.Count + 1,
+                newGame.Name,
+                newGame.Genre,
+                newGame.Price,
+                newGame.ReleaseDate
+            );
+
+            games.Add(game);
+
+            return Results.CreatedAtRoute(EndpointName, new {id = game.Id}, game);
+        });
+
+        // PUT /games/1
+        group.MapPut("/{id}", (int id, UpdateGameDto updatedGame) =>
+        {
+            var index = games.FindIndex(game => game.Id == id);
+
+            if (index == -1)
+            {
+                return Results.NotFound();
+            }
+
+            games[index] = new GameDto(
+                id,
+                updatedGame.Name,
+                updatedGame.Genre,
+                updatedGame.Price,
+                updatedGame.ReleaseDate
+            );
+
+            return Results.NoContent();
+        });
+
+        // DELETE /games/1
+        group.MapDelete("/{id}", (int id) =>
+        {
+            games.RemoveAll(game => game.Id == id);
+
+            return Results.NoContent();
+        });
+    } 
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
-using GameStore.Api.Dtos;
+using GameStore.Api.Endpoints;
 
-const string EndpointName = "GetGame";
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,71 +7,6 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-List<GameDto> games = [
-    new (1, "Street Fighter 2", "Fighting", 19.99M, new DateOnly(1992, 7, 15)),
-    new (2, "Placeholder Game A", "Action", 29.99M, new DateOnly(2000, 1, 1)),
-    new (3, "Placeholder Game B", "Adventure", 39.99M, new DateOnly(2001, 2, 2)),
-    new (4, "Placeholder Game C", "RPG", 49.99M, new DateOnly(2002, 3, 3)),
-    new (5, "Placeholder Game D", "Shooter", 59.99M, new DateOnly(2003, 4, 4)),
-    new (6, "Placeholder Game E", "Puzzle", 9.99M, new DateOnly(2004, 5, 5)),
-    new (7, "Placeholder Game F", "Sports", 19.99M, new DateOnly(2005, 6, 6)),
-    new (8, "Placeholder Game G", "Racing", 29.99M, new DateOnly(2006, 7, 7)),
-    new (9, "Placeholder Game H", "Strategy", 39.99M, new DateOnly(2007, 8, 8)),
-    new (10, "Placeholder Game I", "Simulation", 49.99M, new DateOnly(2008, 9, 9))
-];
-
-app.MapGet("/games", () => games);
-
-// GET /games/1
-app.MapGet("/games/{id}", (int id) =>
-{
-    var game = games.Find(game => game.Id ==id);
-
-    return game is null ? Results.NotFound() : Results.Ok(game);
-}).WithName(EndpointName);
-
-app.MapPost("/games", (CreateGameDto newGame) =>
-{
-    GameDto game = new(
-        games.Count + 1,
-        newGame.Name,
-        newGame.Genre,
-        newGame.Price,
-        newGame.ReleaseDate
-    );
-
-    games.Add(game);
-
-    return Results.CreatedAtRoute(EndpointName, new {id = game.Id}, game);
-});
-
-// PUT /games/1
-app.MapPut("/games/{id}", (int id, UpdateGameDto updatedGame) =>
-{
-    var index = games.FindIndex(game => game.Id == id);
-
-    if (index == -1)
-    {
-        return Results.NotFound();
-    }
-
-    games[index] = new GameDto(
-        id,
-        updatedGame.Name,
-        updatedGame.Genre,
-        updatedGame.Price,
-        updatedGame.ReleaseDate
-    );
-
-    return Results.NoContent();
-});
-
-// DELETE /games/1
-app.MapDelete("/games/{id}", (int id) =>
-{
-    games.RemoveAll(game => game.Id == id);
-
-    return Results.NoContent();
-});
+app.MapGamesEndpoints();
 
 app.Run();
