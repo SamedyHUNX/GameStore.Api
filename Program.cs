f31f26730e79a012e8dfbd8d195b65dbaf36193d
samedyhunx <samedy.hunx@gmail.com>
2026-01-09 02:09:17 +0700

    addgamestoredb

--- Program.cs
+++ Program.cs
@@ -1,28 +1,11 @@
 using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
-using GameStore.Api.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
-
-var connString = "Data Source=GameStore.db";
-builder.Services.AddSqlite<GameStoreContext>(connString, optionsAction: options => options.UseSeeding((context, _) =>
-{
-    if (!context.Set<Genre>().Any())
-    {
-        context.Set<Genre>().AddRange(
-            new Genre {Name = "Fighting"}
-            , new Genre {Name = "Shooter"}
-            , new Genre {Name = "RPG"}
-            , new Genre {Name = "Platformer"}
-            , new Genre {Name = "Puzzle"}
-        );
-
-        context.SaveChanges();
-    }
-}));
+builder.AddGameStoreDb();
 
 builder.Services.AddOpenApi();
 
@@ -31,5 +14,6 @@ var app = builder.Build();
 app.MapGamesEndpoints();
 
 app.MigrateDb();
+
 
 app.Run();
